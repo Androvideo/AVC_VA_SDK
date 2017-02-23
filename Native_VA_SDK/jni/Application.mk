d838b347f9fc9eb897e69b569868648afbb19293
louischenbh <bh_chen@androvideo.com>
2017-02-23 14:41:16 +0800

    Update Application.mk
    It will make NDK build a release version library when adding this option to your application.mk.
    Especially for applications that use OpenCV4Android should add this option for better performance.

--- Native_VA_SDK/jni/Application.mk
+++ Native_VA_SDK/jni/Application.mk
@@ -1,4 +1,6 @@
 APP_ABI := armeabi-v7a
+# Uncomment the line below to your applcation.mk for release build.
+#APP_OPTIM := release
 APP_STL := gnustl_static
 APP_PLATFORM := android-18
 APP_CPPFLAGS := -frtti -fexceptions
