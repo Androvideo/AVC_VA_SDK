9861884f244563232ebccf5efa67bb86a5707051
apo <apo@androvideo.com>
2018-01-18 15:29:15 +0800

    Update buildTool and gradle config

---
+++ VA_Motion_Sample/obj/local/armeabi-v7a/objs/va_motion_sample/va_motion_sample.o.d
@@ -0,0 +1,9 @@
+obj/local/armeabi-v7a/objs/va_motion_sample/va_motion_sample.o: \
+ jni/va_motion_sample.cpp jni/va_interface_v1.h jni/va_const.h \
+ jni/AdvMotionTrack.h
+
+jni/va_interface_v1.h:
+
+jni/va_const.h:
+
+jni/AdvMotionTrack.h:
